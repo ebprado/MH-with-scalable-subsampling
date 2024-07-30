f3bb70b4393e4e0a420053639755448f93aa7045
Estevão <44358323+ebprado@users.noreply.github.com>
2024-07-30 12:13:22 +0100

    Road casualties in the UK

--- 05_road_casualties_UK/car_accidents.py
+++ 05_road_casualties_UK/car_accidents.py
@@ -81,7 +81,7 @@ vector_loop = 'loop'
 # ------------------------------------------------------------------------------------
 # Tuna
 # ------------------------------------------------------------------------------------
-tuna = MH_SS(y, x, V, x0 = theta_hat, control_variates=False, bound = 'new', model=model, phi_function = 'original', chi=1e-7, nburn=nburn, npost=npost_tuna, kappa=0.19, nthin=1000)
+tuna = MH_SS(y, x, V, x0 = theta_hat, control_variates=False, bound = 'new', model=model, phi_function = 'original', chi=1e-7, nburn=nburn, npost=npost_tuna, kappa=0.19, nthin=1000, implementation=vector_loop)
 
 # ------------------------------------------------------------------------------------
 # MH-SS, SMH and RWM
