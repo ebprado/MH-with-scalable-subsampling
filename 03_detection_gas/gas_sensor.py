e0a1f49ce61342fa1cf7aee2f4364686306eb33e
Estevão <44358323+ebprado@users.noreply.github.com>
2024-07-30 11:38:23 +0100

    Detection of gas mixtures

--- 03_detection_gas/gas_sensor.py
+++ 03_detection_gas/gas_sensor.py
@@ -10,7 +10,7 @@ import numpy as np
 from sklearn.linear_model import LogisticRegression
 # --------------------------------------------------------------------------
 # make sure you have the algorithms.py file in the current directory!
-import algorithms 
+from algorithms import *
 # --------------------------------------------------------------------------
 
 # Link to the UCI machine learning repository 
