13306232939f32f60879bdfe0b829747eada8ea8
Estevão <44358323+ebprado@users.noreply.github.com>
2024-07-30 11:26:28 +0100

    Detection of gas mixtures

--- 03_detection_gas/gas_sensor.py
+++ 03_detection_gas/gas_sensor.py
@@ -65,85 +65,84 @@ V = open_file(V_file_name)
 
 nburn = 0
 npost = 100000
+vector_loop = 'loop'
 
 # ------------------------------------------------------------------------------------
-# Original Tuna (only for logistic and poisson regression!!!)
+# Tuna (only for logistic and poisson regression)
 # ------------------------------------------------------------------------------------
-tuna_orig = tunaMH(y, x, V, x0 = theta_hat, control_variates=False, bound = 'new', model=model, phi_function = 'original', chi=7e-6, nburn=nburn, npost=npost, kappa=0.025)
-tuna_orig.get('lambda')
-tuna_orig.get('acc_rate')
+tuna = MH_SS(y, x, V, x0 = theta_hat, control_variates=False, bound = 'new', model=model, phi_function = 'original', chi=7e-6, nburn=nburn, npost=npost, kappa=0.025, implementation=vector_loop)
+tuna.get('lambda')
+tuna.get('acc_rate')
 
 # ------------------------------------------------------------------------------------
-# Tuna+CV, SMH and RWM
+# MH-SS, SMH and RWM
 # ------------------------------------------------------------------------------------
-mh_ss_1 = tunaMH(y, x, V, x0 = theta_hat, control_variates=True, taylor_order=1, bound = 'new', model=model, chi=0, nburn=nburn, npost=npost, kappa=1.5)
-mh_ss_2 = tunaMH(y, x, V, x0 = theta_hat, control_variates=True, taylor_order=2, bound = 'new', model=model, chi=0, nburn=nburn, npost=npost, kappa=1.5)
-smh1 = smh(y, x, V, x0=theta_hat, kappa=1, bound='orig', taylor_order = 1, model=model, nburn=nburn, npost=npost)
-smh2 = smh(y, x, V, x0=theta_hat, kappa=2, bound='orig', taylor_order = 2, model=model, nburn=nburn, npost=npost)
-rwm = RWM(y, x, V, x0=theta_hat, model=model, nburn=nburn, npost=npost, kappa = 2.4)
-
-vector_loop = 'vectorised'
+mhss1 = MH_SS(y, x, V, x0 = theta_hat, control_variates=True, taylor_order=1, bound = 'new', model=model, chi=0, nburn=nburn, npost=npost, kappa=1.5, implementation=vector_loop)
+mhss2 = MH_SS(y, x, V, x0 = theta_hat, control_variates=True, taylor_order=2, bound = 'new', model=model, chi=0, nburn=nburn, npost=npost, kappa=1.5, implementation=vector_loop)
+smh1 = SMH(y, x, V, x0=theta_hat, kappa=1, bound='orig', taylor_order = 1, model=model, nburn=nburn, npost=npost, implementation=vector_loop)
+smh2 = SMH(y, x, V, x0=theta_hat, kappa=2, bound='orig', taylor_order = 2, model=model, nburn=nburn, npost=npost, implementation=vector_loop)
+rwm = RWM(y, x, V, x0=theta_hat, model=model, nburn=nburn, npost=npost, kappa = 2.4, implementation=vector_loop)
 
 tuna_file_name = str(dataset) + model + vector_loop + '_Tuna' + '.pickle'
-mh_ss_1_file_name = str(dataset) + model + vector_loop + '_mh_ss_1' + '.pickle'
-mh_ss_2_file_name = str(dataset) + model + vector_loop + '_mh_ss_2' + '.pickle'
+mhss1_file_name = str(dataset) + model + vector_loop + '_mhss1' + '.pickle'
+mhss2_file_name = str(dataset) + model + vector_loop + '_mhss2' + '.pickle'
 smh1_file_name = str(dataset) + model + vector_loop + '_SMH1' + '.pickle'
 smh2_file_name = str(dataset) + model + vector_loop + '_SMH2' + '.pickle'
 rwm_file_name = str(dataset) + model + vector_loop + '_RWM' + '.pickle'
 
-save_file(tuna_orig, tuna_file_name)
-save_file(mh_ss_1, mh_ss_1_file_name)
-save_file(mh_ss_2, mh_ss_2_file_name)
+save_file(tuna, tuna_file_name)
+save_file(mhss1, mhss1_file_name)
+save_file(mhss2, mhss2_file_name)
 save_file(smh1, smh1_file_name)
 save_file(smh2, smh2_file_name)
 save_file(rwm, rwm_file_name)
 
-tuna_orig = open_file(tuna_file_name)
-mh_ss_1 = open_file(mh_ss_1_file_name)
-mh_ss_2 = open_file(mh_ss_2_file_name)
+tuna = open_file(tuna_file_name)
+mhss1 = open_file(mhss1_file_name)
+mhss2 = open_file(mhss2_file_name)
 smh1 = open_file(smh1_file_name)
 smh2 = open_file(smh2_file_name)
 rwm = open_file(rwm_file_name)
 
 # Acceptance rate ------------------
-tuna_acc_rate = tuna_orig.get('acc_rate')
-mh_ss_1_acc_rate = mh_ss_1.get('acc_rate')
-mh_ss_2_acc_rate = mh_ss_2.get('acc_rate')
+tuna_acc_rate = tuna.get('acc_rate')
+mhss1_acc_rate = mhss1.get('acc_rate')
+mhss2_acc_rate = mhss2.get('acc_rate')
 smh1_acc_rate = smh1.get('acc_rate')
 smh2_acc_rate = smh2.get('acc_rate')
 rwm_acc_rate = rwm.get('acc_rate')
 
 # E(B)/N --------------------------- 
-tuna_EB = np.mean(tuna_orig.get('BoverN'))*N
-mh_ss_1_EB = np.mean(mh_ss_1.get('BoverN'))*N
-mh_ss_2_EB = np.mean(mh_ss_2.get('BoverN'))*N
+tuna_EB = np.mean(tuna.get('BoverN'))*N
+mhss1_EB = np.mean(mhss1.get('BoverN'))*N
+mhss2_EB = np.mean(mhss2.get('BoverN'))*N
 smh1_EB = np.mean(smh1.get('BoverN'))*N
 smh2_EB = np.mean(smh2.get('BoverN'))*N
 
 # ESS per second -------------------
-tuna_ess = np.mean(tuna_orig.get('ESS') / tuna_orig.get('cpu_time'))
-mh_ss_1_ess = np.mean(mh_ss_1.get('ESS') / mh_ss_1.get('cpu_time'))
-mh_ss_2_ess = np.mean(mh_ss_2.get('ESS') / mh_ss_2.get('cpu_time'))
+tuna_ess = np.mean(tuna.get('ESS') / tuna.get('cpu_time'))
+mhss1_ess = np.mean(mhss1.get('ESS') / mhss1.get('cpu_time'))
+mhss2_ess = np.mean(mhss2.get('ESS') / mhss2.get('cpu_time'))
 smh1_ess = np.mean(smh1.get('ESS') / smh1.get('cpu_time'))
 smh2_ess = np.mean(smh2.get('ESS') / smh2.get('cpu_time'))
 rwm_ess = np.mean(rwm.get('ESS') / rwm.get('cpu_time'))
 
 # ESS / E(B) --------------------
-tuna_ess_EB = np.mean(tuna_orig.get('ESS') / tuna_EB)
-mh_ss_1_ess_EB = np.mean(mh_ss_1.get('ESS') / mh_ss_1_EB)
-mh_ss_2_ess_EB = np.mean(mh_ss_2.get('ESS') / mh_ss_2_EB)
+tuna_ess_EB = np.mean(tuna.get('ESS') / tuna_EB)
+mhss1_ess_EB = np.mean(mhss1.get('ESS') / mhss1_EB)
+mhss2_ess_EB = np.mean(mhss2.get('ESS') / mhss2_EB)
 smh1_ess_EB = np.mean(smh1.get('ESS') / smh1_EB)
 smh2_ess_EB = np.mean(smh2.get('ESS') / smh2_EB)
 rwm_ess_EB = np.mean(rwm.get('ESS') / N)
 
-# Metrics
+# Metrics --------------------
 tuna_save_results = np.array([model,  'Tuna',    tuna_acc_rate,  tuna_EB,  tuna_ess,  tuna_ess_EB])
-mh_ss_1_save_results = np.array([model, 'MH-SS-1', mh_ss_1_acc_rate, mh_ss_1_EB, mh_ss_1_ess, mh_ss_1_ess_EB])
-mh_ss_2_save_results = np.array([model, 'MH-SS-2', mh_ss_2_acc_rate, mh_ss_2_EB, mh_ss_2_ess, mh_ss_2_ess_EB])
+mhss1_save_results = np.array([model, 'MH-SS-1', mhss1_acc_rate, mhss1_EB, mhss1_ess, mhss1_ess_EB])
+mhss2_save_results = np.array([model, 'MH-SS-2', mhss2_acc_rate, mhss2_EB, mhss2_ess, mhss2_ess_EB])
 smh1_save_results = np.array([model,  'SMH-1',   smh1_acc_rate,  smh1_EB,  smh1_ess,  smh1_ess_EB])
 smh2_save_results = np.array([model,  'SMH-2',   smh2_acc_rate,  smh2_EB,  smh2_ess,  smh2_ess_EB])
 rwm_save_results = np.array([model,   'RWM',     rwm_acc_rate,   N,        rwm_ess,   rwm_ess_EB])
 
 colnames = ['Model', 'Algorithm', 'Acc. rate', 'E(B)', 'ESSs', 'ESS/E(B)']
-save_results = np.array([tuna_save_results, mh_ss_1_save_results, mh_ss_2_save_results, smh1_save_results, smh2_save_results, rwm_save_results])
+save_results = np.array([tuna_save_results, mhss1_save_results, mhss2_save_results, smh1_save_results, smh2_save_results, rwm_save_results])
 save_results = pd.DataFrame(save_results, columns = colnames)
